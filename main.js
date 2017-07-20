56ba33ff6bbd92d7db8952997e900e339b4c71c8
Adrian Rodriguez <adrianwritenow@gmail.com>
2017-07-20 18:06:25 -0400

    finished based calc

--- main.js
+++ main.js
@@ -8,28 +8,28 @@ var template = `
     </table>
     <table>
     <tr>
-      <td> <input type="button" value="7" onclick = "getUserInput(value)"></td>
-      <td> <input type="button" value="8" onclick = "getUserInput(value)"></td>
-      <td> <input type="button" value="9" onclick = "getUserInput(value)"></td>
-      <td><span> <input class = "operator" type="button" value="/" onclick = "getUserInput(value)"></span></td>
+      <td> <input class = "numButton" type="button" value="7" ></td>
+      <td> <input class = "numButton" type="button" value="8" ></td>
+      <td> <input class = "numButton" type="button" value="9"></td>
+      <td><span> <input class = "operator" type="button" value="/" ></span></td>
     </tr>
     <tr>
-      <td> <input type="button" value="4" onclick = "getUserInput(value)"></td>
-      <td> <input type="button" value="5" onclick = "getUserInput(value)"></td>
-      <td> <input type="button" value="6" onclick = "getUserInput(value)"></td>
-      <td><span> <input class = "operator" type="button" value="X" onclick = "getUserInput(value)"></span></td>
+      <td> <input class = "numButton" type="button" value="4"></td>
+      <td> <input class = "numButton" type="button" value="5"></td>
+      <td> <input class = "numButton" type="button" value="6"></td>
+      <td><span> <input class = "operator" type="button" value="*" ></span></td>
     </tr>
     <tr>
-      <td> <input type="button" value="1" onclick = "getUserInput(value)"></td>
-      <td> <input type="button" value="2" onclick = "getUserInput(value)"></td>
-      <td> <input type="button" value="3" onclick = "getUserInput(value)"></td>
-      <td><span> <input class = "operator" type="button" value="-" onclick = "getUserInput(value)"></span></td>
+      <td> <input class = "numButton" type="button" value="1" ></td>
+      <td> <input class = "numButton" type="button" value="2" ></td>
+      <td> <input class = "numButton" type="button" value="3" ></td>
+      <td><span> <input class = "operator" type="button" value="-" ></span></td>
     </tr>
     <tr>
-      <td> <input type="button" value="0" onclick = "getUserInput(value)"></td>
-      <td> <input type="button" value="." onclick = "getUserInput(value)"></td>
-      <td> <input class = "equalsBtn"type="button" value="=" onclick = "getUserInput(value)"></td>
-      <td><span> <input class = "operator" type="button" value="+" onclick = "getUserInput(value)"></span></td>
+      <td> <input class = "numButton" type="button" value="0" ></td>
+      <td> <input class = "numButton" type="button" value="." ></td>
+      <td> <input class = "equalsBtn" type="button" value="=" ></td>
+      <td><span> <input class = "operator" type="button" value="+"></span></td>
     </tr>
 
   </table>
@@ -38,18 +38,82 @@ var template = `
   let main = document.querySelector(".main").innerHTML = template;
   let calculatorValues = document.querySelector(".buttonCard");
   let numField = document.querySelector(".numField");
+  var numButtons = document.querySelectorAll(".numButton");
+  var clearInput = document.querySelector(".clearBtn")
+  var operators = document.querySelectorAll(".operator");
+  var equals = document.querySelector(".equalsBtn")
+  console.log(numButtons);
 
 
-  var userButton = [];
+var userButton = [];
 
-function getUserInput(value){
+let getUserInput = function(event){
   // userButton.forEach(function(buttonValue){
-    userButton.push(value);
+
+    // alert(event.target.value);
+    userButton.push(event.target.value);
+    numField.innerHTML += " "+event.target.value;
     console.log(userButton);
-    console.log("value is ", value);
-    numField.innerHTML += " "+value;
-// });
+
 }
+let clearUserInput = function(event){
+  userButton = [];
+  numField.innerHTML = " ";
+}
+
+let doMath = function(event){
+  userButton.push(event.target.value);
+  numField.innerHTML += " "+event.target.value;
+}
+
+let getAnswer = function(event){
+  numField.innerHTML = " ";
+  for (var i = 0; i < userButton.length; i++) {
+    if (isNaN(parseInt(userButton[i])) !== isNaN(NaN)) {
+        userButton[i] =  parseInt(userButton[i]);
+        console.log(userButton);
+    }
+    if (userButton.includes("+")){
+    let operatorIndex = userButton.indexOf('+');
+    var value = (userButton[operatorIndex -1] + userButton[operatorIndex +1]);
+    numField.innerHTML = value;
+    }
+
+    if (userButton.includes("-")){
+    let operatorIndex = userButton.indexOf('-');
+    var value = (userButton[operatorIndex -1] - userButton[operatorIndex +1]);
+    numField.innerHTML = value;
+    }
+
+    if (userButton.includes("*")){
+    let operatorIndex = userButton.indexOf('*');
+    var value = (userButton[operatorIndex -1] * userButton[operatorIndex +1]);
+    numField.innerHTML = value;
+    }
+    if (userButton.includes("/")){
+    let operatorIndex = userButton.indexOf('/');
+    var value = (userButton[operatorIndex -1] / userButton[operatorIndex +1]);
+    numField.innerHTML = value;
+    }
+    console.log(userButton);
+  }
+
+
+
+
+}
+equals.addEventListener("click", getAnswer);
+clearInput.addEventListener("click",clearUserInput);
+
+
+for (var i = 0; i < numButtons.length; i++) {
+  numButtons[i].addEventListener("click", getUserInput);
+}
+for (var i = 0; i < operators.length; i++) {
+  operators[i].addEventListener("click", doMath);
+}
+
+
 // var userButton= document.getElementsByTagName('button').value;
 // console.log(userButton);
 
