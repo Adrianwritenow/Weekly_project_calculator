3211df388a7061214c1088b067e0b75ca694a6a9
Adrian Rodriguez <adrianwritenow@gmail.com>
2017-07-21 16:11:17 -0400

    submission 1.0

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ var template = `
       <td><span> <input class = "operator" type="button" value="-" ></span></td>
     </tr>
     <tr>
-      <td> <input class = "numButton" type="button" value="0" ></td>
+      <td> <input class = "numButton" type= "button" value = "0" ></td>
       <td> <input class = "numButton" type="button" value="." ></td>
       <td> <input class = "equalsBtn" type="button" value="=" ></td>
       <td><span> <input class = "operator" type="button" value="+"></span></td>
@@ -42,59 +42,96 @@ var numButtons = document.querySelectorAll(".numButton");
 var clearInput = document.querySelector(".clearBtn")
 var operators = document.querySelectorAll(".operator");
 var equals = document.querySelector(".equalsBtn")
-console.log(numButtons);
 
 
 var userButton = [];
+console.log(userButton);
 
-let getUserInput = function(event){
+let getUserInput = function(event) {
   userButton.push(event.target.value);
-  numField.innerHTML += " "+event.target.value;
+  numField.innerHTML += "" + event.target.value;
+
+  for (var i = 0; i < userButton.length; i++) {
+    if (parseInt(userButton[i - 1]) && parseInt(userButton[i]) || userButton[i] === "0") {
+      value = userButton[i - 1] + userButton[i];
+      console.log(userButton);
+      userButton.pop()
+      userButton.splice(userButton[1 + i], userButton[i]);
+      userButton.pop()
+      userButton.push(value);
+    }
+  }
   console.log(userButton);
 }
 
-let clearUserInput = function(event){
+let clearUserInput = function(event) {
   userButton = [];
   numField.innerHTML = " ";
 }
 
-let doMath = function(event){
+let doMath = function(event) {
   userButton.push(event.target.value);
-  numField.innerHTML += " "+event.target.value;
+  numField.innerHTML += " " + event.target.value;
+  console.log(userButton)
 }
 
-let getAnswer = function(event){
+let getAnswer = function(event) {
   numField.innerHTML = " ";
   for (var i = 0; i < userButton.length; i++) {
-    if (isNaN(parseInt(userButton[i])) !== isNaN(NaN)) {
-      userButton[i] =  parseInt(userButton[i]);
+    if (parseInt(userButton[i])) {
+      userButton[i] = parseInt(userButton[i]);
+    }
+    //working on elminiating functions to a switch case//
+
+    // if (userButton.includes("*"||"/"||"+"||"-")){
+    //   let operatorIndex = userbutton.indexOf();
+    //   var value = (userButton[operatorIndex - 1])  (parseInt(userButton[operatorIndex+1]));
+    //   numField.innerHTML = value;
+    //   userButton.splice(0,3);
+    //   userButton.push(value);
+    //   console.log(userButton);
+    // }
+
+    // completes the problem based on operator//
+    if (userButton.includes("*")) {
+      let operatorIndex = userButton.indexOf('*');
+      var value = (userButton[operatorIndex - 1] * parseInt(userButton[operatorIndex + 1]));
+      numField.innerHTML = value;
+      userButton.splice(0, 3);
+      userButton.push(value);
       console.log(userButton);
     }
-    if (userButton.includes("+")){
-      let operatorIndex = userButton.indexOf('+');
-      var value = (userButton[operatorIndex -1] + userButton[operatorIndex +1]);
-      numField.innerHTML = value;
-    }
-    if (userButton.includes("-")){
-      let operatorIndex = userButton.indexOf('-');
-      var value = (userButton[operatorIndex -1] - userButton[operatorIndex +1]);
-      numField.innerHTML = value;
-    }
-    if (userButton.includes("*")){
-      let operatorIndex = userButton.indexOf('*');
-      var value = (userButton[operatorIndex -1] * userButton[operatorIndex +1]);
-      numField.innerHTML = value;
-    }
-    if (userButton.includes("/")){
+    if (userButton.includes("/")) {
       let operatorIndex = userButton.indexOf('/');
-      var value = (userButton[operatorIndex -1] / userButton[operatorIndex +1]);
+      var value = (userButton[operatorIndex - 1] / parseInt(userButton[operatorIndex + 1]));
       numField.innerHTML = value;
+      userButton.splice(0, 3);
+      userButton.push(value);
+      console.log(userButton);
     }
-    console.log(userButton);
+    if (userButton.includes("+")) {
+      let operatorIndex = userButton.indexOf("+");
+      var value = (userButton[operatorIndex - 1] + parseInt(userButton[operatorIndex + 1]));
+      numField.innerHTML = value;
+      userButton.splice(0, 3);
+      userButton.push(value);
+      console.log(userButton);
+    }
+    if (userButton.includes("-")) {
+      let operatorIndex = userButton.indexOf('-');
+      var value = (userButton[operatorIndex - 1] - parseInt(userButton[operatorIndex + 1]));
+      numField.innerHTML = value;
+      userButton.splice(0, 3);
+      userButton.push(value);
+      console.log(userButton);
+    }
   }
 }
+
+
 equals.addEventListener("click", getAnswer);
-clearInput.addEventListener("click",clearUserInput);
+
+clearInput.addEventListener("click", clearUserInput);
 
 for (var i = 0; i < numButtons.length; i++) {
   numButtons[i].addEventListener("click", getUserInput);
